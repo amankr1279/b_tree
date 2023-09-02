d416e333579db8c1216ace3bb7f137b27468211a
amankr1279 <workaman1138@gmail.com>
2023-09-03 00:00:40 +0530

    Add tests for update

--- main_test.go
+++ main_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 type args struct {
-	val int
+	val    int
+	oldVal int
+	newVal int
 }
 
 type testObj struct {
@@ -23,11 +25,11 @@ func TestInsert(t *testing.T) {
 	tests := make([]testObj, 0)
 	for i := 0; i < 2*T-1; i++ {
 		name := fmt.Sprintf("success_leaf_add_%v", i)
-		tests = append(tests, unitTestMaker(name, args{rand.Intn(10000)}, nil))
+		tests = append(tests, unitTestMaker(name, args{val: rand.Intn(10000)}, nil))
 	}
 	for i := 0; i < 2*T-1; i++ {
 		name := fmt.Sprintf("success_node_add_%v", i)
-		tests = append(tests, unitTestMaker(name, args{rand.Intn(10000)}, nil))
+		tests = append(tests, unitTestMaker(name, args{val: rand.Intn(10000)}, nil))
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -36,7 +38,7 @@ func TestInsert(t *testing.T) {
 			}
 		})
 	}
-	bTree.PrintTree(bTree.root)
+	bTree.PrintTree()
 }
 
 func unitTestMaker(testName string, testAgrs args, want any) testObj {
@@ -62,15 +64,14 @@ func Test_bTree_Search(t *testing.T) {
 		nums = append(nums, x)
 		bTree.Insert(x)
 	}
-	bTree.PrintTree(bTree.root)
+	bTree.PrintTree()
 	for i := 0; i < len(nums); i++ {
 		name := fmt.Sprintf("success_find_%v", i+1)
 		tests = append(tests, unitTestMaker(name, args{val: nums[i]}, true))
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tr := bTree
-			got, err := tr.Search(tt.args.val)
+			got, err := bTree.Search(tt.args.val)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("bTree.Search() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -80,4 +81,31 @@ func Test_bTree_Search(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestBTree_Update(t *testing.T) {
+	bTree := NewBTree()
+	nums := make([]int, 0)
+	for i := 0; i < 3*T; i++ {
+		x := rand.Intn(100)
+		nums = append(nums, x)
+		bTree.Insert(x)
+	}
+	// bTree.PrintTree()
+	fmt.Printf("Nums : %v\n", nums)
+	tests := make([]testObj, 0)
+	for i := 0; i < 5; i++ {
+		name := fmt.Sprintf("success_update_%v", i+1)
+		tests = append(tests, unitTestMaker(name, args{oldVal: nums[i], newVal: rand.Intn(50) + 100}, true))
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if err := bTree.Update(tt.args.oldVal, tt.args.newVal); (err != nil) != tt.wantErr {
+				t.Errorf("BTree.Update() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+	bTree.PrintTree()
 }
